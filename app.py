0227610666a5a6d111c2b61fc129b71e2b2aefca
Meric Ali Yigit <mericaliyigit@gmail.com>
2021-07-23 13:30:01 +0100

    First Commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__,
+            static_url_path='',
+            static_folder='static',
+            template_folder='templates')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
+
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method =='POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_del = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_del)
+        db.session.commit()
+        return redirect('/')
+
+    except:
+        return 'There was a problem deleting the task'
+
+
+@app.route('/update/<int:id>',methods =['POST','GET'])
+def update(id):
+
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error updating the task'
+
+
+
+    else:
+        return render_template('update.html',task=task)
+
+if __name__=="__main":
+    app.run(debug=True)
